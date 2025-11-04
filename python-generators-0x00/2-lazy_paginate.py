ba30aac39b7f3300ba20afcb0ba380fbe9ac3fcb
simon geletaw <simongeletaw2@gmail.com>
2025-11-04 13:41:34 -0400

    paginate user

--- python-generators-0x00/2-lazy_paginate.py
+++ python-generators-0x00/2-lazy_paginate.py
@@ -1,8 +1,8 @@
 from seed import connect_to_db
-def paginate_users(page_size, offset=0):
+def paginate_users(page_size, offset):
     connection=connect_to_db()
     cursor=connection.cursor(dictionary=True)
-    cursor.execute(f"SELECT * FROM alx_prodev.user_data LIMIT {page_size} OFFSET {offset}")
+    cursor.execute(f"SELECT * FROM user_data LIMIT {page_size} OFFSET {offset}")
     rows=cursor.fetchall()
     for row in rows:
         yield row
