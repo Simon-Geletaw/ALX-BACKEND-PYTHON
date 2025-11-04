14c8962b24583f5443aa9df437d20d807d382dab
simon geletaw <simongeletaw2@gmail.com>
2025-11-04 11:59:42 -0400

    stream 6 user from the table

--- python-generators-0x00/0-stream_users.py
+++ python-generators-0x00/0-stream_users.py
@@ -1,6 +1,6 @@
 from seed import connect_db
 import mysql.connector
-def stream_user():
+def stream_users():
     connection = connect_db()
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM alx_prodev.user_data  limit 6;")
@@ -9,6 +9,6 @@ def stream_user():
         yield row
     cursor.close()
     connection.close()
-rows = stream_user()
+rows = stream_users()
 for row in rows:
     print(row)
