b673cc4eddb3e86645950977c83c4fcf14b1fab0
simon geletaw <simongeletaw2@gmail.com>
2025-11-13 09:02:22 -0400

    decorater

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,14 +1,63 @@
 import sqlite3
 import functools
+import logging
+import csv
+import uuid
 from datetime import datetime
+
+# --- Decorator ---
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f"[{datetime.now()}] Executing: {func.__name__}")
+        result = func(*args, **kwargs)
+        print(f"[{datetime.now()}] Finished: {func.__name__}")
+        return result
+    return wrapper
+
+
+# --- Fetch data from SQLite ---
 @log_queries
 def fetch_all_users(query):
     conn = sqlite3.connect('users.db')
-    print("connected successfully") 
-    cursor=conn.cursor()
+    print("Connected successfully") 
+    cursor = conn.cursor()
     cursor.execute(query)
-    results=cursor.fetchall()
+    results = cursor.fetchall()
     conn.close()
     return results
-users=fetch_all_users(query='SELECT * FROM users')
-print(users)
+
+
+def inser_data():
+    con=sqlite3.connect('users.db')
+    cursor=con.cursor()
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+            user_id CHAR(36) NOT NULL,
+            name VARCHAR(100) NOT NULL,
+            email VARCHAR(100) NOT NULL,
+            age INT NOT NULL,
+            PRIMARY KEY (user_id));
+     """)
+    print("Successfully created")
+def insert_data(data):
+    connection=sqlite3.connect('users.db')
+    try:
+        cursor = connection.cursor()
+        with open(data,'r')as file:
+            reader=csv.DictReader(file)
+            for row in reader:
+                user_id=str(uuid.uuid4())
+                name=row['name']
+                email=row['email']
+                age=int(row['age'])
+                cursor.execute("""
+                    INSERT INTO users(user_id, name, email, age)
+                    VALUES (?, ?, ?, ?)
+                """, (user_id, name, email, age))
+
+            connection.commit()
+            print("connection commited")
+    except Exception as e:
+        print(e)
+results=fetch_all_users('select * from users')
