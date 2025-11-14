60abaef6a90c232f7c0caa681219f17a1b01e155
simon geletaw <simongeletaw2@gmail.com>
2025-11-14 07:39:17 -0400

    this using flask

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,10 @@
+#!/usr/bin/env python3
+
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
+
+
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
@@ -9,4 +13,5 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
+        
         
