b20eb80c4d5793815b03634b3c0d7b416665e4a6
simon geletaw <simongeletaw2@gmail.com>
2025-11-14 15:19:04 -0400

    assertRaises egde case

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import unittest
-from parameterized import parameterized,assertRaises
+from parameterized import parameterized
 from utils import access_nested_map
 
 
@@ -13,8 +13,8 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map using parameterized decorator."""
-        self.assertEqual(access_nested_map(nested_map, path), expected)  
-            
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+                   
     @parameterized.expand([
         ({}, ("a",)),
         ({"a": 1}, ("a", "b")),])
@@ -23,4 +23,4 @@ class TestAccessNestedMap(unittest.TestCase):
             access_nested_map(nested_map, path)
 
     # Verify the exception key message (Python formats KeyError as "'key'")
-            self.assertEqual(str(context.exception), repr(path[-1]))
+        self.assertEqual(str(context.exception), repr(path[-1]))
