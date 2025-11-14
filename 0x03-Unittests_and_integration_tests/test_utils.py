7838d6aaca0a45e2265c5a612fd48f578fc229f5
simon geletaw <simongeletaw2@gmail.com>
2025-11-14 14:11:40 -0400

    error egde test

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import unittest
-from parameterized import parameterized
+from parameterized import parameterized,assertRaises
 from utils import access_nested_map
 
 
@@ -13,4 +13,14 @@ class TestAccessNestedMap(unittest.TestCase):
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map using parameterized decorator."""
-        self.assertEqual(access_nested_map(nested_map, path), expected) 
+        self.assertEqual(access_nested_map(nested_map, path), expected)  
+            
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),])
+    def test_access_nested_map_exception(self, nested_map, path):
+        with self.assertRaises(KeyError) as context:
+            access_nested_map(nested_map, path)
+
+    # Verify the exception key message (Python formats KeyError as "'key'")
+            self.assertEqual(str(context.exception), repr(path[-1]))
