00f3d45fb3cccf57df10f82b49b724029d1f1cb2
simon geletaw <simongeletaw2@gmail.com>
2025-11-17 18:24:18 -0400

    test memoized function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,7 +18,7 @@ class TestAccessNestedMap(unittest.TestCase):
 
     @parameterized.expand([
         ({}, ("a",)),
-        ({"a": 1}, ("a", "b")),])
+        ({"a": 1}, ("a", "b")), ])
     def test_access_nested_map_exception(self, nested_map, path):
         with self.assertRaises(KeyError) as context:
             access_nested_map(nested_map, path)
