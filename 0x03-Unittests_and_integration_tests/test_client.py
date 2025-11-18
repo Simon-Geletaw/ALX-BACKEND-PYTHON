a1c9682b729efe68a24e751e2438d03ff1854795
simon geletaw <simongeletaw2@gmail.com>
2025-11-17 20:04:29 -0400

    githubclient test

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,21 @@
+from client import GithubOrgClient
+from unittest import patch
+from parameterized import parameterized
+import unittest
+
+
+class TestGithubClient(unittest.TestCase):
+    @parameterized.expand([("google",),
+                           "abc",])
+    @patch('client.get_json')
+    def test_org(self, org, mock_test):
+        mock_test.return_value = {"login": org}
+        client = GithubOrgClient(org)
+        result = client.org
+        self.assertEqual(result, {"login": org})
+
+        mock_test.assert_called_once_with(f"https://api.github.com/orgs/{org}")
+
+
+if __name__ == '__main__':
+    unittest.main()
