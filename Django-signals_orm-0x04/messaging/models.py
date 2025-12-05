24e15200d80acccefdef1c6a28c5a3f975bb01a8
simon geletaw <simongeletaw2@gmail.com>
2025-12-05 09:25:44 -0400

    this is i dont what i do last night just some kind of problem
    this will create signal when content of message is edited and will write it to the new model called Messageistory

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save ,pre_save
 from django.dispatch import receiver
 
 
@@ -20,7 +20,13 @@ class Message(models.model):
     receiver = models.ForeignKey('User', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    is_edited = models.BooleanField(default=False)
 
+
+class MessageHistory(models.Model):
+    user = models.ForeignKey('User',on_delete=models.CASCADE)
+    oldcontent = models.TextField()
+    
 @receiver(post_save, sender=Message)
 def _post_save_receiver(sender, **kwargs):
     message = Message.kwargs.get('instance')
@@ -31,7 +37,14 @@ def _post_save_receiver(sender, **kwargs):
         receiver=receiver,
         notification_type="New Message"
     )
-    
-    
-    
-    
+
+@receiver(pre_save, sender=Message)
+def update_message_content_history(sender, instance, **kwargs):
+    message = Message.kwargs.get('instance')
+    user = message.sender
+    old_content = message.content
+    if Message.getattr('is_edited', sender=user):
+        MessageHistory.objects.create(
+            user=user,
+            oldcontent=old_content
+        )
