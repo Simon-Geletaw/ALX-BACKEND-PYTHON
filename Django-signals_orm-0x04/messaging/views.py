8e570e84880527f3f271dba21ab29110067b7ac8
simon geletaw <simongeletaw2@gmail.com>
2025-12-05 12:17:14 -0400

    view that will read the messages create nested dictionary for the replies and create self relation parent_mesages

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -17,6 +17,30 @@ class delete_user:
             return HttpResponse("User and related data deleted successfully", status=200)
         except User.DoesNotExist:
             return HttpResponse("User not found", status=404)
-        
-        
-# Create your views here.
+
+
+class get_text_message:
+    def build_thread(self, message):
+        return {
+            "id": message.id,
+            "body": message.content,
+            "timestamp": message.created_at,
+            "is_edited": message.is_edited,
+            "replies": [self.build_thread(reply) for reply in message.replies.all()],
+        }
+
+    def get(self, request, sender_id, reciever_id):
+        user = request.user
+        messages = (
+            Message.objects.filter(
+                Q(sender_id=user, reciever_id=reciever_id)
+                | Q(sender_id=reciever_id, reciever_id=user)
+            )
+            .select_related("sender", "reciever", "parent_message")
+            .prefetch_related("replies")
+            .order_by("created_at")
+        )
+        message_threads = [self.build_thread(msg) for msg in messages if msg.parent_message is None]
+        return JsonResponse({"threads": message_threads}, status=200)
+                       
+               
