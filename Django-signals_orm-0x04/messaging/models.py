fa5fca8c45e32bbb0695aa9f2bbf0b1f40125152
simon geletaw <simongeletaw2@gmail.com>
2025-12-05 12:09:24 -0400

    view that will read the messages create nested dictionary for the replies and create self relation parent_mesages
    view that will read the messages create nested dictionary for the replies and create self relation parent_mesages

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -16,11 +16,14 @@ class Notification(models.Model):
   
    
 class Message(models.model):
+    id = models.AutoField(primary_key=True)
     sender = models.ForeignKey('User', on_delete=models.CASCADE)
     receiver = models.ForeignKey('User', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     is_edited = models.BooleanField(default=False)
+    parent_message = models.ForeignKey('self', null=True, blank=True,
+                                       on_delete=models.CASCADE)
 
 
 class MessageHistory(models.Model):
