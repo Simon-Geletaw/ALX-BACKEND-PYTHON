92095736d074adfd8de0a17489d398c34c4b8901
simon geletaw <simongeletaw2@gmail.com>
2025-12-05 10:32:11 -0400

    we created safe delete signal when user is delete its profile will cascadely delte its notification from both the reciever end and sender id all notification all message history

--- Django-signals_orm-0x04/messaging/views.py
+++ Django-signals_orm-0x04/messaging/views.py
@@ -9,8 +9,8 @@ class DeleteUser:
         if user.id != user_id:
             return HttpResponse("Unauthorized", status=401)
         try:
-            user_to_delete = User.objects.get(id=user_id)
-            user_to_delete.delete()
+            user = User.objects.get(id=user_id)
+            user.delete()
             return HttpResponse("User and related data deleted successfully", status=200)
         except User.DoesNotExist:
             return HttpResponse("User not found", status=404)
